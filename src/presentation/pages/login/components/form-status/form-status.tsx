d7619bf112ea5104a521c3c4af0e4f302dcf724f
davimattos <davimattosdev@gmail.com>
2023-05-22 23:32:37 -0300

    feat: create FormStatus internal component

---
+++ src/presentation/pages/login/components/form-status/form-status.tsx
@@ -0,0 +1,17 @@
+import React, {useContext} from 'react';
+import {ActivityIndicator, View, Text} from 'react-native';
+
+import Context from '../../../../context/form-context';
+
+const FormStatus: React.FC = () => {
+  const {state} = useContext<any>(Context);
+
+  return (
+    <View testID="error-wrapper">
+      {state.isLoading && <ActivityIndicator role="spinbutton" />}
+      {state.mainError && <Text testID="main-error">{state.mainError}</Text>}
+    </View>
+  );
+};
+
+export default FormStatus;
